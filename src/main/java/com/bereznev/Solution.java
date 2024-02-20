6682fb9d1561e9f312f7bd65ee2266fd109367e7
Wex <bernikcooldude@yandex.ru>
2024-02-20 22:13:09 +0300

    Day 7/31 Search Insert Position(https://leetcode.com/problems/search-insert-position/)

--- src/main/java/com/bereznev/Solution.java
+++ src/main/java/com/bereznev/Solution.java
@@ -6,6 +6,25 @@ import java.util.*;
 
 @Component
 public class Solution {
+  //todo unit-test
+  // https://leetcode.com/problems/search-insert-position/
+  public int searchInsert(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return left;
+  }
+
   //  https://leetcode.com/problems/valid-parentheses/description/
   public boolean isParenthesesValid(String s) { //NOSONAR
     if (s == null || s.length() % 2 != 0) {
